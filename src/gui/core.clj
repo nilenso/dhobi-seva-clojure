5d03ed00d1bfb864ee8c03e94988358b98c744ed
Mayank <mayankpahwa.com>
2017-02-24 10:46:58 +0530

    Refactored directory structure

--- src/gui/core.clj
+++ src/gui/core.clj
@@ -1,4 +1,4 @@
-(ns dhobi-seva-clojure.core
+(ns gui.core
   (:use [seesaw core])
   (:require [database.core :as database])
   (:require [validate.core :as validate])
@@ -53,7 +53,68 @@
         [(button :id :add-course :text "Add Course" :font {:size 20} :listen [:action handler-add-course]) :grid :next :weightx 1.0]]))
 
 
-(defn -main []
-  (database/main)
-  (-> (main-screen-frame)
-    show!))
+(defn handler-add-student [course-name]
+    (let [data (value (select f [:#form]))
+          student-name (:student-name data)
+          room         (:room data)
+          seat         (:seat data)]
+          (do (database/add-Student course-name student-name room seat)
+              (main-screen-frame))))
+
+
+(defn add-student-frame [course-name]
+  (form-panel :id :form
+      :items [
+        [nil :fill :both :insets (java.awt.Insets. 5 5 5 5) :gridx 0 :gridy 0]
+
+        [(label :text "Course Name: " :halign :center :font {:size 20}) :grid :wrap]
+
+        [(label :text course-name :halign :left :font {:size 20}) :grid :next]
+
+        [[1 :by 1] :grid :wrap]
+
+        [(label :text "Name:" :halign :center :font {:size 20})]
+
+        [(text :columns 20 :id :student-name :font {:size 20}) :grid :next]
+
+        [(label :text "Room:" :halign :center :font {:size 20}) :gridheight 1 :grid :wrap]
+
+        [(text :columns 20 :id :room :font {:size 20}) :grid :next :weightx 1.0]
+
+        [(label :text "Seat:" :halign :center :font {:size 20}) :gridheight 1 :grid :wrap]
+
+        [(text :columns 20 :id :seat :font {:size 20}) :grid :next :weightx 1.0]
+
+        [[1 :by 1] :grid :wrap]
+
+        [(button :id :add-student :text "Add Student" :font {:size 20} 
+                                  :listen [:action (fn [e] (handler-add-student course-name))]) :grid :next :weightx 1.0]]))
+
+
+;(defn -main []
+;  (database/main)
+;  (-> (main-screen-frame)
+;    show!))
+
+
+(defn window1 [content]
+  (frame
+    :title "Resturant Selector"
+    :content content
+    :width 200
+    :height 50
+    :on-close :close))
+
+(defn -main
+  []
+  (let [rating-label (label :text "Please choose rating:")
+        rating (combobox :model ["1 star" "2 star"])
+        location (slider
+                   :value 5 :min 0 :max 20
+                   :minor-tick-spacing 1 :major-tick-spacing 2
+                   :snap-to-ticks? true
+                   :paint-ticks? true :paint-labels? true)
+
+        main-window (window1 (vertical-panel :items [rating-label rating location]))]
+    (-> main-window
+        show!)))
