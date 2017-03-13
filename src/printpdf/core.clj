ada9c5fb2b60f622af82b43eba11dc48a4cce649
Mayank <mayankpahwa.com>
2017-03-13 18:28:54 +0530

    Create layout for single student to be written to pdf

--- src/printpdf/core.clj
+++ src/printpdf/core.clj
@@ -1,2 +1,14 @@
 (ns printpdf.core
+  (:use clj-pdf.core)
+  (:require [database.core :as database])
   (:gen-class))
+
+(defn single-student
+  [course-name student-name]
+  (let [[room seat deposit purchase laundry] (database/student-details course-name student-name)]
+      [:table {:border true}
+      [[:paragraph [:paragraph [:chunk {:style :bold} "Name: "] student-name]
+                   [:paragraph [:chunk {:style :bold} "Room: "] room]
+                   [:paragraph [:chunk {:style :bold} "Deposit: "] deposit]
+                   [:paragraph [:chunk {:style :bold} "Laundry: "] laundry]
+                   [:paragraph [:chunk {:style :bold} "Purchases: "] purchase]]]]))
