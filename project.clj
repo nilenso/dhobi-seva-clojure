73c81076cec4705093713f8bb3c26ddb156505a2
Mayank <mayankpahwa.com>
2017-03-13 15:47:40 +0530

    Add dependencies for pdf generation

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-  				 [seesaw "1.4.4"]
-  				 [org.clojure/data.json "0.2.1"]]
+           [seesaw "1.4.4"]
+           [org.clojure/data.json "0.2.1"]
+           [clj-pdf "2.2.16"]]
   :main ^:skip-aot gui.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
