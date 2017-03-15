cdc5df570c759fb429b51096ff10ff5b5f561908
Mayank <mayankpahwa.com>
2017-03-15 15:52:13 +0530

    Convert string course-name to keyword for fetch/store

--- src/database/core.clj
+++ src/database/core.clj
@@ -15,7 +15,7 @@
     (let [this-course {:start start,
                        :duration duration,
                        :students {}}]
-        (swap! all-course-data assoc-in [course-name] this-course)))
+        (swap! all-course-data assoc-in [(keyword course-name)] this-course)))
 
 
 (defn add-student
@@ -25,17 +25,17 @@
                    :deposit 0,
                    :purchases [],
                    :laundry []}]
-        (swap! all-course-data assoc-in [course-name :students student-name] student)))
+        (swap! all-course-data assoc-in [(keyword course-name) :students student-name] student)))
 
 
 (defn course-exists?
     [course-name]
-      (get-in @all-course-data [course-name]))
+      (get-in @all-course-data [(keyword course-name)]))
 
 
 (defn student-exists?
     [course-name student-name]
-    (get-in @all-course-data [course-name :students student-name]))
+    (get-in @all-course-data [(keyword course-name) :students student-name]))
 
 
 (defn course-list
@@ -61,7 +61,7 @@
 
 (defn student-details
   [course-name student-name]
-  (let [student-data (get-in @all-course-data [course-name :students student-name])
+  (let [student-data (get-in @all-course-data [(keyword course-name) :students student-name])
         room (get student-data :room)
         seat (get student-data :seat)
         deposit (get student-data :deposit)
@@ -78,29 +78,29 @@
 
 (defn add-deposit
     [course-name student-name deposit]
-        (swap! all-course-data assoc-in [course-name :students student-name :deposit] (Integer. deposit)))
+        (swap! all-course-data assoc-in [(keyword course-name) :students student-name :deposit] (Integer. deposit)))
 
 
 (defn add-purchase
     [course-name student-name purchase-name purchase-cost]
         (let [data {:purchase-name purchase-name,
                     :purchase-cost (Integer. purchase-cost)}
-              length (count (get-in @all-course-data [course-name :students student-name :purchases]))]
-        (swap! all-course-data assoc-in [course-name :students student-name :purchases length] data)))
+              length (count (get-in @all-course-data [(keyword course-name) :students student-name :purchases]))]
+        (swap! all-course-data assoc-in [(keyword course-name) :students student-name :purchases length] data)))
 
 
 (defn add-laundry
     [course-name student-name laundry-cost]
         (let [data {:laundry-name "Laundry"
                     :laundry-cost (Integer. laundry-cost)}
-              length (count (get-in @all-course-data [course-name :students student-name :laundry]))]
-        (swap! all-course-data assoc-in [course-name :students student-name :laundry length] data)))
+              length (count (get-in @all-course-data [(keyword course-name) :students student-name :laundry]))]
+        (swap! all-course-data assoc-in [(keyword course-name) :students student-name :laundry length] data)))
 
 
 (defn purchase-list
   [course-name student-name]
   (let [x (atom 0)]
-    (for [all-purchases (get-in @all-course-data [course-name :students student-name :purchases])
+    (for [all-purchases (get-in @all-course-data [(keyword course-name) :students student-name :purchases])
           :let [position (swap! x inc)]]
         (hash-map :serial-number position, 
                   :purchase-name (get all-purchases :purchase-name), 
@@ -110,7 +110,7 @@
 (defn laundry-list
   [course-name student-name]
   (let [x (atom 0)]
-    (for [all-laundry (get-in @all-course-data [course-name :students student-name :laundry])
+    (for [all-laundry (get-in @all-course-data [(keyword course-name) :students student-name :laundry])
           :let [position (swap! x inc)]]
         (hash-map :serial-number position, 
                   :laundry-name "Laundry", 
@@ -119,7 +119,7 @@
 
 (defn all-student-names
   [course-name]
-  (sort (keys (get-in @all-course-data [course-name :students]))))
+  (sort (keys (get-in @all-course-data [(keyword course-name) :students]))))
 
 
 (defn main
