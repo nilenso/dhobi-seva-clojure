035cf047f0f009694462c053140c225904668413
Mayank <mayankpahwa.com>
2017-03-16 10:35:09 +0530

    Added test for add course

--- test/gui/core_test.clj
+++ test/gui/core_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [gui.core :refer :all]))
 
+(def all-course-data (atom {}))
+
+(defn add-course
+    [course-name start duration]
+    (let [this-course {:start start,
+                       :duration duration,
+                       :students {}}]
+        (swap! all-course-data assoc-in [(keyword course-name)] this-course)))
+
+
+(defn add-student
+    [course-name student-name roomNo seatNo]
+    (let [student {:room roomNo,
+                   :seat seatNo,
+                   :deposit 0,
+                   :purchases [],
+                   :laundry []}]
+        (swap! all-course-data assoc-in [(keyword course-name) :students (keyword student-name)] student)))
+
 (deftest a-test
+	(let [data {:Vipassana {:start "2017-03-17", :duration "10", :students {}}}] 
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= data (add-course "Vipassana" "2017-03-17" "10"))))))
